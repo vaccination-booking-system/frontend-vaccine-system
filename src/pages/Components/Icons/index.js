8296c3c60fb2e0dd7a282b28118a31a8d9d368dc
Muhammad Ridwan <mohridwan1707@gmail.com>
2022-06-06 22:33:15 +0700

    feat: add navbar components

---
+++ src/pages/Components/Icons/index.js
@@ -0,0 +1,6 @@
+import CubeIcon from "./Cube";
+import { ProfileIcon } from "./Profile";
+import ImageIcon from "./Image";
+import KeyIcon from "./Key";
+
+export { CubeIcon, ProfileIcon, ImageIcon, KeyIcon };
---
+++ src/pages/Components/Navbar/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Logo from "../Logo";
+import PropsTypes from "prop-types";
+
+// Navbar Icons
+import { CubeIcon, ProfileIcon, ImageIcon, KeyIcon } from "../Icons";
+
+const navItems = [
+  { text: "Dashboard", icon: ({ color }) => <CubeIcon color={color} />, alt: "dashboard-icon" },
+  { text: "About", icon: ({ color }) => <ProfileIcon color={color} />, alt: "about-icon" },
+  { text: "Gallery", icon: ({ color }) => <ImageIcon color={color} />, alt: "gallery-icon" },
+  { text: "Contact Us", icon: ({ color }) => <KeyIcon color={color} />, alt: "contact-icon" },
+];
+
+/**
+ *
+ * @param {string} path - path the url location of navbar
+ * example path === "login" or "register"
+ *
+ */
+const Navbar = ({ path }) => {
+  return (
+    <div
+      className={`flex justify-between items-center p-4 rounded-md ${
+        path === "login" ? "bg-slate-100 shadow-md" : path === "register" && "text-white"
+      }  m-6`}
+    >
+      <Logo {...(path === "register" && { color: "white" })} />
+      <ul className="flex">
+        {navItems.map((item, idx) => (
+          <li key={idx} className="flex items-center mx-2">
+            <div className="my-auto">
+              <item.icon {...(path === "register" && { color: "white" })} />
+            </div>
+            <p className="mx-2">{item.text}</p>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <button
+          className={`${
+            path === "register" ? "text-[#151928] bg-white" : path === "login" && "text-white bg-[#151928]"
+          } py-2 px-10 rounded-full text-[12px] font-bold`}
+        >
+          SIGN IN
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  path: PropsTypes.string.isRequired,
+};
+
+export default Navbar;
