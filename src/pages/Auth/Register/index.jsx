750b4aefcef873144c4534cfaf0063e5ddae33dd
Muhammad Ridwan <mohridwan1707@gmail.com>
2022-06-10 08:03:24 +0700

    refactor: register page

--- src/pages/Auth/Register/index.jsx
+++ src/pages/Auth/Register/index.jsx
@@ -39,10 +39,8 @@ const Register = () => {
 
   return (
     <>
-      <div>
-        {/* ini div navbar atau header */}
-        <Navbar path="register" />
-      </div>
+      {/* ini div navbar atau header */}
+      <Navbar path="register" />
       <div className="flex flex-col justify-center items-center h-[790px] p-6">
         {/* ini div table form */}
         <div className="h-[50%] w-[100%] bg-cover rounded-[15px] bg-bottom relative" style={{ backgroundImage: `url(${BackgroundRegister})` }}>
@@ -61,102 +59,100 @@ const Register = () => {
         <div className="h-[50%] bg-white w-[100%]"></div>
         <div className="z-20 drop-shadow-md absolute mt-[130px]">
           <Card>
-            <div className="">
-              <div className="font-bold px-[50px] pt-[75px] text-[25px]">Buat Akun Baru</div>
-              <div>
-                <form onSubmit={handleSubmit}>
-                  <div>
-                    <div className="flex gap-x-12 px-[50px] pt-[42px]">
+            <div className="font-bold px-[50px] pt-[75px] text-[25px]">Buat Akun Baru</div>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <div className="flex gap-x-12 px-[50px] pt-[42px]">
+                    <div>
                       <div>
-                        <div>
-                          <label htmlFor="nameForm">Name</label>
-                        </div>
-                        <div className="pt-[6px]">
-                          <input
-                            type="text"
-                            placeholder="Your Full Name"
-                            name="nameForm"
-                            className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
-                            style={{ width: "370px", height: "50px" }}
-                            value={name}
-                            title="Isikan Nama Lengkap Anda"
-                            onChange={event => handleInputChange(event)}
-                            autoComplete="off"
-                          />
-                        </div>
+                        <label htmlFor="nameForm">Name</label>
                       </div>
+                      <div className="pt-[6px]">
+                        <input
+                          type="text"
+                          placeholder="Your Full Name"
+                          name="nameForm"
+                          className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
+                          style={{ width: "370px", height: "50px" }}
+                          value={name}
+                          title="Isikan Nama Lengkap Anda"
+                          onChange={event => handleInputChange(event)}
+                          autoComplete="off"
+                        />
+                      </div>
+                    </div>
+                    <div>
                       <div>
-                        <div>
-                          <label htmlFor="nikForm">Nomor Induk Kependudukan</label>
-                        </div>
-                        <div className="pt-[6px]">
-                          <input
-                            type="text"
-                            placeholder="Your NIK"
-                            name="nikForm"
-                            className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
-                            style={{ width: "370px", height: "50px" }}
-                            value={nik}
-                            title="Isikan NIK Anda"
-                            onChange={event => handleInputChange(event)}
-                            maxLength={16}
-                            autoComplete="off"
-                          />
-                        </div>
+                        <label htmlFor="nikForm">Nomor Induk Kependudukan</label>
+                      </div>
+                      <div className="pt-[6px]">
+                        <input
+                          type="text"
+                          placeholder="Your NIK"
+                          name="nikForm"
+                          className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
+                          style={{ width: "370px", height: "50px" }}
+                          value={nik}
+                          title="Isikan NIK Anda"
+                          onChange={event => handleInputChange(event)}
+                          maxLength={16}
+                          autoComplete="off"
+                        />
                       </div>
                     </div>
                   </div>
-                  <div>
-                    <div className="flex gap-x-12 pl-[50px] pt-[24px]">
+                </div>
+                <div>
+                  <div className="flex gap-x-12 pl-[50px] pt-[24px]">
+                    <div>
                       <div>
-                        <div>
-                          <label htmlFor="number_phoneForm">Nomor Telepon</label>
-                        </div>
-                        <div className="pt-[6px]">
-                          <input
-                            type="text"
-                            placeholder="Your Phone Number"
-                            name="number_phoneForm"
-                            className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
-                            style={{ width: "370px", height: "50px" }}
-                            value={phoneNumber}
-                            title="Isikan Nomor Telepon Anda"
-                            onChange={event => handleInputChange(event)}
-                            maxLength={13}
-                            autoComplete="off"
-                          />
-                        </div>
+                        <label htmlFor="number_phoneForm">Nomor Telepon</label>
                       </div>
+                      <div className="pt-[6px]">
+                        <input
+                          type="text"
+                          placeholder="Your Phone Number"
+                          name="number_phoneForm"
+                          className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
+                          style={{ width: "370px", height: "50px" }}
+                          value={phoneNumber}
+                          title="Isikan Nomor Telepon Anda"
+                          onChange={event => handleInputChange(event)}
+                          maxLength={13}
+                          autoComplete="off"
+                        />
+                      </div>
+                    </div>
+                    <div>
                       <div>
-                        <div>
-                          <label htmlFor="passwordForm">Password</label>
-                        </div>
-                        <div className="pt-[6px]">
-                          <input
-                            type="password"
-                            placeholder="Your Password"
-                            name="passwordForm"
-                            className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
-                            style={{ width: "370px", height: "50px" }}
-                            value={password}
-                            title="Isikan Password Anda"
-                            onChange={event => handleInputChange(event)}
-                          />
-                        </div>
+                        <label htmlFor="passwordForm">Password</label>
+                      </div>
+                      <div className="pt-[6px]">
+                        <input
+                          type="password"
+                          placeholder="Your Password"
+                          name="passwordForm"
+                          className="rounded-[15px] pl-[20px] border-2 border-[#E2E8F0]"
+                          style={{ width: "370px", height: "50px" }}
+                          value={password}
+                          title="Isikan Password Anda"
+                          onChange={event => handleInputChange(event)}
+                        />
                       </div>
                     </div>
                   </div>
-                  <div className="flex justify-center pt-[50px]">
-                    <button className="bg-black w-[370px] h-[45px] rounded-[12px] border-2 border-[#E2E8F0]" type="submit">
-                      <div className="text-white text-[10px] font-bold">DAFTAR</div>
-                    </button>
-                  </div>
-                </form>
-                <div className="text-[14px] flex justify-center gap-1 pt-6">
-                  <div className="text-[#A0AEC0]">Already have an account? </div>
-                  <div className="font-bold text-[#C4C4C4]">
-                    <a href="#">Sign In</a>
-                  </div>
+                </div>
+                <div className="flex justify-center pt-[50px]">
+                  <button className="bg-black w-[370px] h-[45px] rounded-[12px] border-2 border-[#E2E8F0]" type="submit">
+                    <div className="text-white text-[10px] font-bold">DAFTAR</div>
+                  </button>
+                </div>
+              </form>
+              <div className="text-[14px] flex justify-center gap-1 pt-6">
+                <div className="text-[#A0AEC0]">Already have an account? </div>
+                <div className="font-bold text-[#C4C4C4]">
+                  <a href="#">Sign In</a>
                 </div>
               </div>
             </div>
