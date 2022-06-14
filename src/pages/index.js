8cb348a979723ade459d58f6b5f2b2f2f6a6936c
Muhammad Ridwan <mohridwan1707@gmail.com>
2022-06-14 13:37:08 +0700

    fix: change directory name Dasboard to Dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 
-import Dashboard from "./Dasboard";
-import VaccinationBookingsPage from "./Dasboard/VaccinationBookings";
-import AddFamilyMemberPage from "./Dasboard/AddFamilyMember";
-import TicketVaccinePage from "./Dasboard/TicketVaccine";
+import Dashboard from "./Dashboard";
+import VaccinationBookingsPage from "./Dashboard/VaccinationBookings";
+import AddFamilyMemberPage from "./Dashboard/AddFamilyMember";
+import TicketVaccinePage from "./Dashboard/TicketVaccine";
 
 export { Login, Register, Dashboard, VaccinationBookingsPage, AddFamilyMemberPage, TicketVaccinePage };
