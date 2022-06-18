bc834b63be195f272ee94d75e471ccf26d242a76
riioaditya <rioadityano@gmail.com>
2022-06-18 15:10:19 +0900

    fix: fix bug components icons and layout

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="h-[100vh] overflow-y-scroll py-12 px-8 flex-1 bg-[#DBF5FE]">{children}</div>
+      <div className="h-[100vh] overflow-y-auto py-6 px-8 flex-1 bg-[#DBF5FE]">{children}</div>
     </div>
   );
 };
