0d44cebbb31eac1ba126bd85fc5fbd6f66603d55
riioaditya <rioadityano@gmail.com>
2022-07-14 23:03:18 +0900

    feat: can delete family member and get data family members

--- src/pages/AddFamilyMember/index.jsx
+++ src/pages/AddFamilyMember/index.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
-import { Button, Card, Layout, Sidebar } from "../../Components";
+import { Button, Card, Layout, LoadingAnimation, Sidebar } from "../../Components";
 import { AiOutlineEye } from "react-icons/ai";
 import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import Footer from "../../Components/Footer";
 import { useSelector } from "react-redux";
+import axiosInstance from "../../network/apis";
+import Swal from "sweetalert2";
+import { decodeToken } from "react-jwt";
 
 const AddFamilyMemberPage = () => {
   const [isClicked, setIsClicked] = useState(false);
+
+  const [listFamilyMember, setListFamilyMember] = useState([]);
 
   const { getUserByIdLoading, getUserByIdResult, getUserByIdError } = useSelector(state => state.userId);
 
@@ -25,6 +29,62 @@ const AddFamilyMemberPage = () => {
       }
     }, 1800);
   };
+
+  const [isGetData, setIsGetData] = useState(false);
+
+  //console.log(getUserByIdResult.id);
+  const fetchData = () => {
+    setIsGetData(true);
+    const token = localStorage.getItem("accessToken");
+    const claim = decodeToken(token);
+    console.log("ini claim" + claim);
+    axiosInstance
+      .get("/api/v1/family-members?user_id=" + claim.user_id, {
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      })
+      .then(function (response) {
+        let temp = [];
+        response.data.data.forEach(element => {
+          temp.push(element);
+        });
+        setListFamilyMember(temp);
+        console.log(response);
+        setIsGetData(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setIsGetData(false);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleDelete = id => {
+    axiosInstance
+      .delete("/api/v1/family-members/" + id, {
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then(response => {
+        console.log(response);
+        if (response.data.message === "SUCCESS") {
+          Swal.fire({
+            title: "SUCCESS !",
+            text: "Berhasil menghapus",
+            icon: "success",
+            confirmButtonText: "Close",
+          });
+          fetchData();
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <Layout>
@@ -34,12 +94,12 @@ const AddFamilyMemberPage = () => {
               <div className="pl-[15px]">
                 {/* Ini Div Header */}
                 <div className="pb-[5px] pt-[20px]">
-                  <span className="text-xs font-normal leading-[1.4] text-[#718096] cursor-pointer" onClick={handleClickedContent}>
+                  <span className="font-normal leading-[1.4] text-[#718096] cursor-pointer" onClick={handleClickedContent}>
                     Add Family Member{" "}
                   </span>
                 </div>
                 <div className="pb-[25px]">
-                  <span className="text-lg font-bold">Add Family Members</span>
+                  <span className="text-[20px] font-bold">Add Family Members</span>
                 </div>
               </div>
             </div>
@@ -56,7 +116,7 @@ const AddFamilyMemberPage = () => {
                       </div>
                       <div className="pt-[17px]">
                         <div className="text-[#EDF5FB] text-base leading-[1.3]">
-                          <p className="font-semibold">Muhammad Faidhorrahman</p>
+                          <p className="font-semibold">{getUserByIdResult.name}</p>
                           <p className="font-normal">Akun Anda</p>
                         </div>
                       </div>
@@ -69,48 +129,36 @@ const AddFamilyMemberPage = () => {
                     </div>
                     <div>
                       {/* ini div tampung list nama */}
-                      <ul className="list-decimal pl-[15px]">
-                        <li className="pb-[25px]">
-                          <div className="flex justify-between">
-                            <span>Muhammad Faidhorrahman</span>
-                            <div className="flex gap-x-5 cursor-pointer pr-2">
-                              <div className="">
-                                <Link to="/dashboard/add-family-member/view">
-                                  <AiOutlineEye size={22} />
-                                </Link>
-                              </div>
-                              <div>
-                                <Link to="/dashboard/add-family-member/edit">
-                                  <BiEdit size={22} />
-                                </Link>
-                              </div>
-                              <div>
-                                <MdDelete size={22} />
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-                        <li className="pb-[25px]">
-                          <div className="flex justify-between">
-                            <span>Muhammad Faidhorrahman</span>
-                            <div className="flex gap-x-5 cursor-pointer pr-2">
-                              <div className="">
-                                <Link to="/view">
-                                  <AiOutlineEye size={22} />
-                                </Link>
-                              </div>
-                              <div>
-                                <Link to="/edit">
-                                  <BiEdit size={22} />
-                                </Link>
-                              </div>
-                              <div>
-                                <MdDelete size={22} />
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
+                      {isGetData ? (
+                        <LoadingAnimation />
+                      ) : (
+                        <ul className="list-decimal pl-[15px]">
+                          {listFamilyMember.map((element, index) => {
+                            return (
+                              <li className="pb-[25px]" key={index}>
+                                <div className="flex justify-between">
+                                  <span>{element.name}</span>
+                                  <div className="flex gap-x-5 cursor-pointer pr-2">
+                                    <div className="">
+                                      <Link to={`/family-member/view/${element.id}`}>
+                                        <AiOutlineEye size={22} />
+                                      </Link>
+                                    </div>
+                                    <div>
+                                      <Link to={`/family-member/edit/${element.id}`}>
+                                        <BiEdit size={22} />
+                                      </Link>
+                                    </div>
+                                    <div>
+                                      <MdDelete size={22} onClick={() => handleDelete(element.id)} />
+                                    </div>
+                                  </div>
+                                </div>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      )}
                     </div>
                   </div>
                   {/* <div className="flex justify-center">
@@ -120,7 +168,7 @@ const AddFamilyMemberPage = () => {
                       </button>
                     </div> */}
                   <div className="flex justify-center mt-[30px]">
-                    <Link to="/add-family-member/add">
+                    <Link to="/family-member/add">
                       <Button btnSize="lg" fontSize="12px" bg="#0A6C9D" color="white">
                         + TAMBAH ANGGOTA
                       </Button>
@@ -132,7 +180,7 @@ const AddFamilyMemberPage = () => {
           </>
         ) : getUserByIdLoading ? (
           <div className="absolute w-full h-full flex justify-center items-center">
-            <p>Loading..</p>
+            <LoadingAnimation />
           </div>
         ) : (
           ""
