f897ff8e220d7f2649d0eacafdff2fb74ebbafc3
riioaditya <rioadityano@gmail.com>
2022-07-14 23:02:20 +0900

    feat: create add family member feature

--- src/pages/AddFamilyMember/AddMembers/index.jsx
+++ src/pages/AddFamilyMember/AddMembers/index.jsx
@@ -1,41 +1,164 @@
 import React from "react";
 import { Button, Card, Layout, Sidebar } from "../../../Components";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import axiosInstance from "../../../network/apis";
+import Swal from "sweetalert2";
 
 const AddMembers = () => {
   const [isClicked, setIsClicked] = useState(false);
   const [isFocus, setIfFocus] = useState(false);
 
-  /* const [nama, setNama] = useState("");
+  const [nama, setNama] = useState("");
   const [nik, setNik] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [gender, setGender] = useState("");
   const [relation, setRelation] = useState("");
-  const [birthday, setBirthday] = useState(""); */
+  const [birthday, setBirthday] = useState("");
 
-  const [dataForm, setDataForm] = useState({
-    nama: "",
-    nik: "",
-    birthday: "",
-    phoneNumber: "",
-    gender: "",
-    relation: "",
-  });
+  const [nikErrorMsg, setNikErrorMsg] = useState("");
+  const [phoneNumberErrorMsg, setPhoneNumberErrorMsg] = useState("");
+  const [nameErrorMsg, setNameErroMsg] = useState("");
+  const [birthdayErrorMsg, setBirthdayErrorMsg] = useState("");
+  const [relationErrorMsg, setRelationErrorMsg] = useState("");
+  const [genderErrorMsg, setGenderErrorMsg] = useState("");
+
+  const checkIsFormError = () => {
+    return (
+      nama === "" ||
+      nik === "" ||
+      phoneNumber === "" ||
+      gender === "" ||
+      relation === "" ||
+      birthday === "" ||
+      nikErrorMsg !== "" ||
+      phoneNumberErrorMsg !== "" ||
+      nameErrorMsg !== "" ||
+      birthdayErrorMsg !== "" ||
+      relationErrorMsg !== "" ||
+      genderErrorMsg !== ""
+    );
+  };
+
+  const navigate = useNavigate();
 
   const handleSubmit = e => {
     e.preventDefault();
-    /* setDataForm({
-      nama: nama,
-      nik: nik,
-      phoneNumber: phoneNumber,
-      gender: gender,
-      relation: relation,
-      birthday: birthday,
-    }); */
-    //console.log(dataForm);
-    alert("data berhasil ditambahkan");
+
+    axiosInstance
+      .post(
+        "/api/v1/family-members",
+        {
+          name: nama,
+          date_of_birth: birthday,
+          nik,
+          gender,
+          relationship: relation,
+          phone_number: phoneNumber,
+        },
+        {
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      )
+      .then(function (response) {
+        if (nama !== "" && nik !== "" && birthday !== "" && gender !== "" && relation !== "" && phoneNumber !== "") {
+          Swal.fire({
+            title: "SUCCESS !",
+            text: "Thank you for your request.",
+            icon: "success",
+            confirmButtonText: "Close",
+          });
+          setNama("");
+          setBirthday("");
+          setGender("");
+          setNik("");
+          setPhoneNumber("");
+          setRelation("");
+          navigate("/family-member");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        if (error.response.status === 400) {
+          Swal.fire({
+            title: "Data Not Valid!",
+            text: "Please input the correct data",
+            icon: "error",
+            confirmButtonText: "Close",
+          });
+        }
+      });
+  };
+
+  const checkNama = value => {
+    const regexNama = /^[^-\0-9][a-zA-Z ]*$/gim;
+
+    if (value.length > 0 && regexNama.test(value)) {
+      //console.log("tes1");
+      setNameErroMsg("");
+    } else if (value.length > 0 && !regexNama.test(value)) {
+      setNameErroMsg("Nama tidak sesuai format");
+    }
+
+    if (value.length === 0) {
+      console.log("tes3");
+      setNameErroMsg("");
+    }
+  };
+
+  const checkNikLength = value => {
+    const regexNik = /^[0-9]*$/;
+    if ((value.length > 0 && value.length < 16) || !regexNik.test(value)) {
+      setNikErrorMsg("NIK Tidak Valid");
+    } else if ((value.length === 16 || value.length === 0) && nikErrorMsg !== "") {
+      setNikErrorMsg("");
+    }
+  };
+
+  const checkNumberPhone = value => {
+    const regexPhone = /^08+[0-9]*$/;
+    if (value.length > 0 && !regexPhone.test(value)) {
+      setPhoneNumberErrorMsg("Input dimulai dengan 08 & harus berupa angka");
+      console.log("test 1");
+    } else if (value.length > 1 && regexPhone.test(value)) {
+      setPhoneNumberErrorMsg("");
+      console.log("test 2");
+      if (value.length < 12) {
+        setPhoneNumberErrorMsg("Nomor HP kurang");
+        console.log("test 3");
+      }
+    }
+    if (value.length === 0) {
+      setPhoneNumberErrorMsg("");
+      console.log("test 4");
+    }
+  };
+
+  const checkGender = value => {
+    if (value === "") {
+      setGenderErrorMsg("Mohon masukkan jenis kelamin Anda");
+    } else {
+      setGenderErrorMsg("");
+    }
+  };
+
+  const checkBirthday = value => {
+    if (value === "") {
+      setBirthdayErrorMsg("Mohon masukkan Tanggal Lahir Anda");
+    } else {
+      setBirthdayErrorMsg("");
+    }
+  };
+
+  const checkRelation = value => {
+    if (value === "") {
+      setRelationErrorMsg("Mohon Masukkan Hubungan Anda");
+    } else {
+      setRelationErrorMsg("");
+    }
   };
 
   const handleClickedContent = () => {
@@ -57,7 +180,41 @@ const AddMembers = () => {
     }
   };
 
-  //console.log(dataForm);
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+
+    if (name === "nama") {
+      checkNama(value);
+      setNama(value);
+      console.log(value);
+    }
+    if (name === "nik") {
+      checkNikLength(value);
+      setNik(value);
+      console.log(value);
+    }
+    if (name === "phoneNumber") {
+      checkNumberPhone(value);
+      setPhoneNumber(value);
+      console.log(value);
+    }
+    if (name === "birthday") {
+      checkBirthday(value);
+      setBirthday(value);
+      console.log(value);
+    }
+    if (name === "gender") {
+      checkGender(value);
+      setGender(value);
+      console.log(value);
+    }
+    if (name === "relation") {
+      checkRelation(value);
+      setRelation(value);
+      console.log(value);
+    }
+  };
+
   return (
     <>
       <Layout>
@@ -65,8 +222,8 @@ const AddMembers = () => {
           <div className="pl-[15px]">
             {/* Ini Div Header */}
             <div className="pb-[5px] pt-[20px]">
-              <span className="text-xs font-normal leading-[1.4] text-[#718096]">
-                <Link to="/add-family-member">Add Family Member</Link>
+              <span className=" font-normal leading-[1.4] text-[#718096]">
+                <Link to="/family-member">Add Family Member</Link>
                 <span className="text-[#0A6C9D] cursor-pointer" onClick={handleClickedContent}>
                   / Identitas
                 </span>
@@ -100,11 +257,12 @@ const AddMembers = () => {
                               placeholder="Masukkan Nama (sesuai KTP)"
                               className="border-2 border-[black] rounded-[15px] py-[15px] px-[20px]"
                               style={{ height: 50, width: "100%" }}
-                              onChange={e => setDataForm({ ...dataForm, [e.target.name]: e.target.value })}
-                              value={dataForm.nama}
+                              onChange={event => handleInputChange(event)}
+                              value={nama}
                               autoComplete="off"
                             />
                           </div>
+                          <div className="pt-1 text-xs text-red-700">{nameErrorMsg}</div>
                         </div>
                         <div className="pb-[32px]">
                           {/* nampung NIK */}
@@ -118,11 +276,12 @@ const AddMembers = () => {
                               placeholder="Masukkan NIK"
                               className="border-2 border-[black] rounded-[15px] py-[15px] px-[20px]"
                               style={{ height: 50, width: "100%" }}
-                              onChange={e => setDataForm({ ...dataForm, [e.target.name]: e.target.value })}
-                              value={dataForm.nik}
+                              onChange={event => handleInputChange(event)}
+                              value={nik}
                               maxLength={16}
                             />
                           </div>
+                          <div className="pt-1 text-xs text-red-700">{nikErrorMsg}</div>
                         </div>
                         <div className="pb-[32px]">
                           {/* nampung gender*/}
@@ -134,14 +293,15 @@ const AddMembers = () => {
                               name="gender"
                               className="text-[#777777] border-2 border-[black] rounded-[15px] px-[20px]"
                               style={{ height: 50, width: "100%" }} /* onChange={() => ()} */
-                              onChange={e => setDataForm({ ...dataForm, [e.target.name]: e.target.value })}
-                              value={dataForm.gender}
+                              onChange={event => handleInputChange(event)}
+                              value={gender}
                             >
-                              <option>Jenis Kelamin Anda</option>
-                              <option value="L">Laki-Laki</option>
-                              <option value="P">Wanita</option>
+                              <option value="">Jenis Kelamin Anda</option>
+                              <option value="M">Laki-Laki</option>
+                              <option value="F">Wanita</option>
                             </select>
                           </div>
+                          <div className="pt-1 text-xs text-red-700">{genderErrorMsg}</div>
                         </div>
                       </div>
                     </div>
@@ -160,13 +320,14 @@ const AddMembers = () => {
                               className={`${isFocus ? "text-[#777777]" : "text-black"} border-2 border-[black] rounded-[15px] px-[20px]`}
                               onFocus={handleOnFocus}
                               style={{ height: 50, width: "100%" }}
-                              onChange={e => setDataForm({ ...dataForm, [e.target.name]: e.target.value })}
-                              value={dataForm.birthday}
+                              onChange={event => handleInputChange(event)}
+                              value={birthday}
                             />
                           </div>
+                          <div className="pt-1 text-xs text-red-700">{birthdayErrorMsg}</div>
                         </div>
                         <div className="pb-[32px]">
-                          {/* nampung no hp */}
+                          {/* nampung nomor hp */}
                           <div className="pb-[6px]">
                             <label htmlFor="phoneNumber">Nomor Telepon</label>
                           </div>
@@ -177,36 +338,48 @@ const AddMembers = () => {
                               placeholder="Masukkan Nomor HP"
                               className="border-2 border-[black] rounded-[15px] py-[15px] px-[20px]"
                               style={{ height: 50, width: "100%" }}
-                              onChange={e => setDataForm({ ...dataForm, [e.target.name]: e.target.value })}
-                              value={dataForm.phoneNumber}
+                              onChange={event => handleInputChange(event)}
+                              value={phoneNumber}
                               maxLength={13}
                             />
                           </div>
+                          <div className="pt-1 text-xs text-red-700">{phoneNumberErrorMsg}</div>
                         </div>
                         <div className="pb-[32px]">
                           {/* nampung hubungan*/}
                           <div className="pb-[6px]">
-                            <label htmlFor="hubungan">Hubungan</label>
+                            <label htmlFor="relation">Hubungan</label>
                           </div>
                           <div>
                             <select
                               name="relation"
                               className="border-2 border-[black] rounded-[15px] py-[10px] px-[20px] text-[#777777]"
                               style={{ height: 50, width: "100%" }}
-                              onChange={e => setDataForm({ ...dataForm, [e.target.name]: e.target.value })}
-                              value={dataForm.relation}
+                              onChange={event => handleInputChange(event)}
+                              value={relation}
                             >
                               <option value="">Hubungan</option>
-                              <option value="keluarga">Keluarga</option>
-                              <option value="teman">Teman</option>
+                              <option value="sibling">Saudara</option>
+                              <option value="parent">Orang Tua</option>
+                              <option value="child">Anak</option>
+                              <option value="other">Lain-Lain</option>
                             </select>
                           </div>
+                          <div className="pt-1 text-xs text-red-700">{relationErrorMsg}</div>
                         </div>
                       </div>
                     </div>
                   </div>
                   <div className="flex justify-center items-center pb-[54.5px]">
-                    <Button btnSize="lg" fontSize="12px" bg="#0A6C9D" color="white" type="submit">
+                    {/* <Button btnSize="lg" fontSize="12px" bg="#0A6C9D" color="white" type="submit"> */}
+                    <Button
+                      btnSize="lg"
+                      cursor={checkIsFormError() ? "not-allowed" : null}
+                      bg={checkIsFormError() ? "#CACACA" : "#0A6C9D"}
+                      color={checkIsFormError() ? "black" : "white"}
+                      fontSize="12px"
+                      type="submit"
+                    >
                       TAMBAHKAN ANGGOTA
                     </Button>
                   </div>
