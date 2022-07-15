b616917504ec421a0b47aa3e7c23525342e1ccf5
KrishnaAryawan <aryawan.krisna@gmail.com>
2022-07-15 17:05:04 +0800

    Add: Font Weight in SectionAboutUs

--- src/Components/LandingPage/SectionAboutUs.jsx
+++ src/Components/LandingPage/SectionAboutUs.jsx
@@ -12,19 +12,19 @@ function SectionAboutUs() {
         <Box height="100px" mt="30vh" pl="5vw">
           <b style={{ fontSize: "28px" }}>Apa itu Evizy?</b>
           <div>
-            <h2 style={{ fontSize: "18px", width: "40vw" }}>
+            <h2 style={{ fontSize: "18px", fontWeight: "600", width: "35vw" }}>
               Evizy adalah aplikasi yang dikembangkan untuk membantu instansi pemerintah terkait dalam melakukan pelacakan untuk menghentikan
               penyebaran Coronavirus Disease (COVID-19).
             </h2>
           </div>
           <div>
-            <h2 style={{ fontSize: "18px", width: "40vw" }}>
+            <h2 style={{ marginTop: "2vh", fontSize: "18px", fontWeight: "600", width: "35vw" }}>
               Aplikasi ini mengandalkan partisipasi masyarakat untuk saling membagikan data lokasinya saat bepergian agar penelusuran riwayat kontak
               dengan penderita COVID-19 dapat dilakukan.
             </h2>
           </div>
           <div>
-            <h2 style={{ fontSize: "18px", width: "40vw" }}>
+            <h2 style={{ marginTop: "2vh", fontSize: "18px", fontWeight: "600", width: "35vw" }}>
               Pengguna aplikasi ini juga akan mendapatkan notifikasi jika berada di keramaian atau berada di zona merah, yaitu area atau kelurahan
               yang sudah terdata bahwa ada orang yang terinfeksi COVID-19 positif atau ada Pasien Dalam Pengawasan.
             </h2>
